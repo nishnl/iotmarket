8ecf12de75be0a895d7b0eb45b9d0627b7314c42
Nish <nishnl@hotmail.com>
2013-05-29 00:00:55 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iotmarket::Application.config.secret_token = '200a8ea6b3c96b2b95f6fc547b2e65f59b109c030fb3ee708430a5e3348d8ed946dbb9176da3e68d85f71b2bb9c1c90174aad46873e1bfa1d32dbb746ad678e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Iotmarket::Application.config.session_store :cookie_store, key: '_iotmarket_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Iotmarket::Application.config.session_store :active_record_store
