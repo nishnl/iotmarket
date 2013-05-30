93abfdcbfba52cd96fa2e8a9ca4db911157ef31c
efremov <eco933@rambler.ru>
2013-05-30 11:25:02 +0400

    fb sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130530044548) do
+ActiveRecord::Schema.define(:version => 20130530064220) do
 
   create_table "list_pins", :force => true do |t|
     t.string   "description"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(:version => 20130530044548) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "provider"
+    t.string   "username"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
